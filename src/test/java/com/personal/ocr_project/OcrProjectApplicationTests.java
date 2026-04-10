7d2a49e893abb3112bc0227aa82220514f30f42a
EpsilonOmegatron <adham852002@hotmail.com>
2026-04-10 20:00:00 +0200

    Initial Commit

---
+++ src/test/java/com/personal/ocr_project/OcrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ocr_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
